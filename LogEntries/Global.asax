ce5d535236524cbddecb5e3d091b90778d38a584
CastellGaby <castellgaby.dubon@unitec.edu>
2015-03-16 18:21:12 -0600

    LogEntries Example

---
+++ LogEntries/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="LogEntries.MvcApplication" Language="C#" %>
---
+++ LogEntries/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(LogEntries.Startup))]
+namespace LogEntries
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
